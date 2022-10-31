889d39726a5a45b8b5990384b655a02217391aaa
JJK70 <jjk701@gmail.com>
2022-10-31 15:25:06 -0400

    bar calculator lab

---
+++ lessons/string_format.py
@@ -0,0 +1,11 @@
+num1 = 3.1425467389
+num2 = 10.2903948
+
+#Previous Method
+# print('num 1 is', num1, 'and num 2 is', num2)
+
+#Fornmat Method
+# print('num 1 is {0:.3f} and num 2 is {1:.3f}'.format(num1, num2))
+
+#Using F-strings
+print(f'num 1 is {num1:.4f} and num2 is {num2:.4f}')
